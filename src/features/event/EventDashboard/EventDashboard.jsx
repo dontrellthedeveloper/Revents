2e5c2c6d0bb0d0342ed9b95a7cb6dbac434e3903
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-07-21 12:07:12 -0700

    Updating & Deleting the events

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,55 +1,55 @@
-import React, { Component } from "react";
-import { Grid, Button } from "semantic-ui-react";
-import EventList from "../EventList/EventList";
-import EventForm from "../EventForm/EventForm";
-import cuid from "cuid";
+import React, { Component } from 'react';
+import { Grid, Button } from 'semantic-ui-react';
+import EventList from '../EventList/EventList';
+import EventForm from '../EventForm/EventForm';
+import cuid from 'cuid';
 
 const eventsFromDashboard = [
   {
-    id: "1",
-    title: "Trip to Tower of London",
-    date: "2018-03-27",
-    category: "culture",
+    id: '1',
+    title: 'Trip to Tower of London',
+    date: '2018-03-27',
+    category: 'culture',
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
-    city: "London, UK",
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
     venue: "Tower of London, St Katharine's & Wapping, London",
-    hostedBy: "Bob",
-    hostPhotoURL: "https://randomuser.me/api/portraits/men/20.jpg",
+    hostedBy: 'Bob',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
     attendees: [
       {
-        id: "a",
-        name: "Bob",
-        photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
       },
       {
-        id: "b",
-        name: "Tom",
-        photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
       }
     ]
   },
   {
-    id: "2",
-    title: "Trip to Punch and Judy Pub",
-    date: "2018-03-28",
-    category: "drinks",
+    id: '2',
+    title: 'Trip to Punch and Judy Pub',
+    date: '2018-03-28',
+    category: 'drinks',
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
-    city: "London, UK",
-    venue: "Punch & Judy, Henrietta Street, London, UK",
-    hostedBy: "Tom",
-    hostPhotoURL: "https://randomuser.me/api/portraits/men/22.jpg",
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
+    city: 'London, UK',
+    venue: 'Punch & Judy, Henrietta Street, London, UK',
+    hostedBy: 'Tom',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
     attendees: [
       {
-        id: "b",
-        name: "Tom",
-        photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
+        id: 'b',
+        name: 'Tom',
+        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
       },
       {
-        id: "a",
-        name: "Bob",
-        photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
+        id: 'a',
+        name: 'Bob',
+        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
       }
     ]
   }
@@ -62,11 +62,11 @@ class EventDashboard extends Component {
     selectedEvent: null
   };
 
-  //   handleIsOpenToggle = () => {
-  //     this.setState(({ isOpen }) => ({
-  //       isOpen: !isOpen
-  //     }));
-  //   };
+  // handleIsOpenToggle = () => {
+  //   this.setState(({ isOpen }) => ({
+  //     isOpen: !isOpen
+  //   }));
+  // };
 
   handleCreateFormOpen = () => {
     this.setState({
@@ -83,7 +83,7 @@ class EventDashboard extends Component {
 
   handleCreateEvent = newEvent => {
     newEvent.id = cuid();
-    newEvent.hostPhotoURL = "/assets/user.png";
+    newEvent.hostPhotoURL = '/assets/user.png';
     this.setState(({ events }) => ({
       events: [...events, newEvent],
       isOpen: false
@@ -97,25 +97,50 @@ class EventDashboard extends Component {
     });
   };
 
+  handleUpdateEvent = updatedEvent => {
+    this.setState(({ events }) => ({
+      events: events.map(event => {
+        if (event.id === updatedEvent.id) {
+          return { ...updatedEvent };
+        } else {
+          return event;
+        }
+      }),
+      isOpen: false,
+      selectedEvent: null
+    }));
+  };
+
+  handleDeleteEvent = id => {
+    this.setState(({ events }) => ({
+      events: events.filter(e => e.id !== id)
+    }));
+  };
+
   render() {
     const { events, isOpen, selectedEvent } = this.state;
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList events={events} selectEvent={this.handleSelectEvent} />
+          <EventList
+            events={events}
+            selectEvent={this.handleSelectEvent}
+            deleteEvent={this.handleDeleteEvent}
+          />
         </Grid.Column>
         <Grid.Column width={6}>
           <Button
             positive
-            content="Create Event"
+            content='Create Event'
             onClick={this.handleCreateFormOpen}
           />
           {isOpen && (
             <EventForm
-            key={selectedEvent ? selectedEvent.id : 0}
+              key={selectedEvent ? selectedEvent.id : 0}
               selectedEvent={selectedEvent}
-              cancelFormOpen={this.handleIsOpenToggle}
-              createEvent={this.handleFormCancel}
+              cancelFormOpen={this.handleFormCancel}
+              createEvent={this.handleCreateEvent}
+              updateEvent={this.handleUpdateEvent}
             />
           )}
         </Grid.Column>
@@ -125,3 +150,4 @@ class EventDashboard extends Component {
 }
 
 export default EventDashboard;
+
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
-import { Segment, Item, Icon, List, Button } from "semantic-ui-react";
-import EventListAttendee from "./EventListAttendee";
+import React, { Component } from 'react';
+import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
+import EventListAttendee from './EventListAttendee';
 
 class EventListItem extends Component {
   render() {
-      const {event, selectEvent} = this.props
+    const {event, selectEvent, deleteEvent} = this.props;
     return (
       <Segment.Group>
         <Segment>
           <Item.Group>
             <Item>
               <Item.Image
-                size="tiny"
+                size='tiny'
                 circular
                 src={event.hostPhotoURL}
               />
               <Item.Content>
-                <Item.Header>{event.title}</Item.Header>
+                <Item.Header as='a'>{event.title}</Item.Header>
                 <Item.Description>
                   Hosted by {event.hostedBy}
                 </Item.Description>
@@ -26,20 +26,21 @@ class EventListItem extends Component {
         </Segment>
         <Segment>
           <span>
-            <Icon name="clock" /> {event.date} |
-            <Icon name="marker" /> {event.venue}
+            <Icon name='clock' /> {event.date} |
+            <Icon name='marker' /> {event.venue}
           </span>
         </Segment>
         <Segment secondary>
           <List horizontal>
             {event.attendees && event.attendees.map(attendee => (
-                <EventListAttendee key={attendee.id} attendee={attendee} />
+              <EventListAttendee key={attendee.id} attendee={attendee} />
             ))} 
           </List>
         </Segment>
         <Segment clearing>
           <span>{event.description}</span>
-          <Button onClick={() => selectEvent(event)} as="a" color="teal" floated="right" content="View" />
+          <Button onClick={() => deleteEvent(event.id)} as='a' color='red' floated='right' content='Delete' />
+          <Button onClick={() => selectEvent(event)} as='a' color='teal' floated='right' content='View' />
         </Segment>
       </Segment.Group>
     );
