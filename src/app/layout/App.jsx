bb0f44d7b8f6f3b8b9af4bfd9a33a00c26fd4ae9
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-07-23 21:57:40 -0700

    Added internal navigation to the app

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -14,22 +14,24 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/(.+)' render={() => (
-        <Fragment>
-          <NavBar />
-          <Container className="main">
-          <Route path='/events' component={EventDashboard} />
-          <Route path='/events/:id' component={EventDetailedPage} />
-          <Route path='/people' component={PeopleDashboard} />
-          <Route path='/profile/:id' component={UserDetailedPage} />
-          <Route path='/settings' component={SettingsDashboard} />
-          <Route path='/createEvent' component={EventForm} />
-          </Container>
-        </Fragment>
-        )} />
+        <Route exact path="/" component={HomePage} />
+        <Route
+          path="/(.+)"
+          render={() => (
+            <Fragment>
+              <NavBar />
+              <Container className="main">
+                <Route path="/events" component={EventDashboard} />
+                <Route path="/events/:id" component={EventDetailedPage} />
+                <Route path="/people" component={PeopleDashboard} />
+                <Route path="/profile/:id" component={UserDetailedPage} />
+                <Route path="/settings" component={SettingsDashboard} />
+                <Route path="/createEvent" component={EventForm} />
+              </Container>
+            </Fragment>
+          )}
+        />
       </Fragment>
- 
     );
   }
 }
